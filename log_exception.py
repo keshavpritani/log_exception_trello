cac68f0a5fd6cbd5f8e08abc6f9e22197040003d
Cloud User <keshav@kristal.ai>
2021-09-07 15:32:37 +0000

    initial commit

---
+++ log_exception.py
@@ -0,0 +1,237 @@
+import requests
+import json
+# import pprint
+from datetime import date, timedelta
+
+# pp = pprint.PrettyPrinter()
+d = ""
+current_date = ""
+last_week = ""
+last_week_id = ""
+
+
+board_id = "612f9677e58e0a87b80c3c40"
+url = "https://api.trello.com/1/boards/wavZwmwo/lists"
+
+base_list_url = "https://api.trello.com/1/lists"
+lists_url = base_list_url+"/{}/{}"
+
+base_card_url = "https://api.trello.com/1/cards"
+
+ignore_list_name = "Ignore Card List"
+
+list_id = ""
+ignore_list_id = "612f969406b71889476adc7a"
+
+query = {
+    'key': '6e5c646bcf9f209c9221cd2fd72a18cf',
+    'token': '1fd1dce81d58308556244cc30a6554f5e5061a285cef486135e9a19ed31c7f83',
+    "fields": ["name"]
+}
+
+ignore_exceptions = []
+already_added_exceptions = {}
+
+def changeDate():
+    global current_date, last_week, d
+    d = date.today()
+    current_date = d.strftime("%d %B, %Y")
+    print("Current Date =", current_date)
+    last_week = (d - timedelta(days=7)).strftime("%d %B, %Y")
+    print("last_week =", last_week)
+    ignore_exceptions = []
+    already_added_exceptions = {}
+    fetchIds()
+
+
+def createNewList():
+    global last_week_id
+
+    if(last_week_id != ""):
+        print("Deleting", last_week, "list")
+        query["value"] = "true"
+        response = requests.request(
+            "PUT",
+            lists_url.format(last_week_id, "closed"),
+            params=query
+        )
+        last_week_id = ""
+        del query["value"]
+
+    query["name"] = current_date
+    query["idBoard"] = board_id
+    response = requests.request(
+        "POST",
+        base_list_url,
+        params=query
+    )
+    print("Crearted new List for Current Date")
+
+    del query["name"]
+    del query["idBoard"]
+
+    a = json.loads(response.text)
+    return a['id']
+
+def updateCard(card_id):
+#    print(card_id)
+    print("Card for Exception Already Exists")
+    # get the card id and update the card description
+    query["fields"].append("desc")
+    response = requests.request(
+        "GET",
+        base_card_url+"/{}".format(card_id),
+        params=query
+    )
+    a = json.loads(response.text)
+    # pp.pprint(a)
+    # split the card description and get the count
+    card_desc = a['desc'].split("\n")
+    count = int(card_desc[0].split(":")[1])
+    count += 1
+    card_desc[0] = "Count: {}".format(count)
+    card_desc = "\n".join(card_desc)
+    # print(card_desc)
+    # update the card description
+    query["desc"] = card_desc
+    response = requests.request(
+        "PUT",
+        base_card_url+"/{}".format(card_id),
+        params=query
+    )
+    query["fields"].remove("desc")
+    del query["desc"]
+    print("Card's Count Incremented")
+
+def createCard(program_name, exception_name, card_desc, exceptions_list):
+    global list_id, ignore_list_id, base_card_url, d, current_date
+
+    exception_name = exception_name.strip(" ")
+    #print("CREATE CARD CALLED")
+    if(d != date.today()):
+        print("New Day")
+        list_id = ""
+        last_week_id = ""
+        changeDate()
+
+    if(exception_name in ignore_exceptions):
+        print("Exception Igonred")
+        return
+
+    response = requests.request(
+        "GET",
+        lists_url.format(ignore_list_id, "cards"),
+        params=query
+    )
+
+    ignore_cards_list = json.loads(response.text)
+
+    for x in ignore_cards_list:
+        if x['name'].find(exception_name) != -1:
+            ignore_exceptions.append(exception_name)
+            print("Exception Igonred")
+            return
+
+    #print(already_added_exceptions)
+
+    if exception_name in already_added_exceptions:
+        #print("hello")
+        updateCard(already_added_exceptions[exception_name])
+        return
+
+    query["name"] = program_name + " - "  + exception_name
+    query["idList"] = list_id
+    query["desc"] = "Count : 1\n" + "Exceptions - " + ", ".join(exceptions_list) + "\n\n"  + card_desc
+    query["pos"]="top"
+    response = requests.request(
+        "POST",
+        base_card_url,
+        params=query
+    )
+    #print("after")
+    if(200 <= response.status_code < 300):
+        already_added_exceptions[exception_name] = json.loads(response.text)['id']
+        print("Created new Exception Card")
+    else:
+        print("Error while Creating Card")
+
+    del query["name"]
+    del query["idList"]
+    del query["desc"]
+    del query["pos"]
+
+def fetchIds():
+    # print(url)
+    global list_id, last_week_id
+    response = requests.request(
+        "GET",
+        url,
+        params=query
+    )
+    a = json.loads(response.text)
+    # pp.pprint(a)
+    for x in a:
+        if x['name'] == current_date:
+            print("List for Current Date Already Exists")
+            list_id = x['id']
+        if x['name'] == last_week:
+            last_week_id = x['id']
+
+    #print(list_id)
+    #print(last_week_id)
+    if list_id == "":
+        list_id = createNewList()
+
+    response = requests.request(
+        "GET",
+        lists_url.format(list_id, "cards"),
+        params=query
+    )
+
+    a = json.loads(response.text)
+    # pp.pprint(a)
+
+    for x in a:
+        t = x['name'].split("-")[-1].strip(" ")
+        already_added_exceptions[t] = x['id']
+#    print(already_added_exceptions)
+
+    response = requests.request(
+        "GET",
+        lists_url.format(ignore_list_id, "cards"),
+        params=query
+    )
+
+    ignore_cards_list = json.loads(response.text)
+
+    for x in ignore_cards_list:
+        ignore_exceptions.append(x['name'].split("-")[-1].strip(" "))
+
+def getBoardAndIgnoreListId():
+    # find the board id and ignore list id by querying the api
+    global board_id, ignore_list_id
+    query["fields"].append("idBoard")
+    response = requests.request(
+        "GET",
+        url,
+        params=query
+    )
+    a = json.loads(response.text)
+    print("Board id = ", a[0]['idBoard'])
+    # find the ignore list id
+    for x in a:
+        if x['name'] == ignore_list_name:
+            print("List Id = ", x['id'])
+            break
+    else:
+        print("Ignore List Not Found")
+
+    query["fields"].remove("idBoard")
+
+
+# check if the file has run by __main__ or imported
+if __name__ == "__main__":
+    getBoardAndIgnoreListId()
+else:
+    changeDate()
+
---
+++ read_log.py
@@ -0,0 +1,64 @@
+import time
+import os
+import log_exception
+import re
+from datetime import datetime
+
+# Set the filename and open the file
+filename = '/var/log/messages'
+file = open(filename, 'r')
+
+# Find the size of the file and move to the end
+def get_file_last_pos():
+    st_results = os.stat(filename)
+    return st_results[6]
+
+file.seek(get_file_last_pos())
+
+log_type = ("ERROR", "WARN", "INFO", "DEBUG")
+
+while 1:
+    try:
+        where = file.tell()
+        line = file.readline()
+        #print(line)
+        #print("Keshav")
+        if not line:
+            time.sleep(0.01)
+            file.seek(where)
+        else:
+            str_to_find = " ERROR "
+            index = line.find(str_to_find)
+            #print(line)
+            if(index != -1):
+                program_name = line.split()[4]
+                #print(program_name)
+                index = index+len(str_to_find)
+                index1 = line.find("-", index)
+                exception = line[index:index1]
+                desc = line[index1+2:]
+                flag=False
+                exceptions_list=set()
+                # if(re.search("exception",line,flags=re.IGNORECASE)):
+                #     flag=True
+                while 1:
+                    where = file.tell()
+                    line = file.readline()
+                    if(any(l_type in line for l_type in log_type)):
+                        file.seek(get_file_last_pos() - 10)
+                        break
+                    if(re.search("exception",line,flags=re.IGNORECASE)):
+                        line_arr = (re.split('[.() :$]', line))
+                        exceptions_list = exceptions_list.union(set([match for match in line_arr if re.search("exception",match,flags=re.IGNORECASE)]))
+                        flag=True
+                    desc = desc + line
+                if flag:
+                    today = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    print(today, "- Exception -", exception)
+                    #print("DESC -", desc)
+                    log_exception.createCard(program_name, exception, desc ,exceptions_list)
+            # else:
+                # print("Not Found")
+    except Exception as e:
+        print("try...catch block")
+        print(e)
