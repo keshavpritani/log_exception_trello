09defef5bb75b904091e523ccee6d8afb2d0fd2d
Your Name <keshav@kristal.ai>
2021-09-11 14:52:25 +0530

    updated restart_script.sh

--- restart_script.sh
+++ restart_script.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-process_id=`ps f | grep read_log | awk 'NR==1 {print $1}'`
-
-if [ process_id != "" ]; then
-	kill -9 process_id
-	nohup python3 /home/centos/log_exception_trello/read_log.py &
-fi
+screen -S log_exception -X quit
+screen -dmS log_exception bash -c '/usr/bin/python3 /home/centos/log_exception_trello/read_log.py'
